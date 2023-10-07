0e876f9b42c9f187e0578783265ec465e837121c
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-10-07 16:31:57 -0700

    [newsAPI] add os env to send email

--- sendEmail.py
+++ sendEmail.py
@@ -1,6 +1,6 @@
 import smtplib
 import ssl
-
+import os
 
 def send_email(message):
     """
@@ -10,10 +10,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = ""
-    password = ""
+    username = os.environ.get("username")
+    password = os.environ.get("password")
 
-    receiver = ""
+    receiver = os.environ.get("receiver")
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
