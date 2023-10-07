6d095e6625bdd509702e3342224ca696959e2fd7
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-10-07 15:57:34 -0700

    [newsAPI] add sendemail and os to get env from os

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests
+from sendEmail import send_email
+import os
 
-API_KEY = ""
-URL = "https://newsapi.org/v2/everything?q=tesla" \
-    "&from=2023-09-07&sortBy=publishedAt&apiKey=" \
-    "71ffe15533b34c179171d17e61c66fce"
+API = os.environ.get('API_KEY')
+TOPIC = "tesla"
+print(API)
+
+URL = "https://newsapi.org/v2/everything?" \
+       f"q={TOPIC}&" \
+      "sortBy=publishedAt&" \
+      f"apiKey={API}&" \
+      "language=en"
+
+print(URL)
 
 # Make request
 request = requests.get(URL, timeout=10)
 
 # Get a dictionary with data
 content = request.json()
+print(content)
+BODY = ""
 
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        BODY = "Subject: Today's NEWS" + "\n" + BODY + article["title"] + "\n" \
+            + article["description"] + "\n"  \
+            + article["url"] + 2*"\n"
 
-for article in content["articles"]:
-    print(article["title"])
+BODY = BODY.encode("utf-8")
+
+send_email(message=BODY)
